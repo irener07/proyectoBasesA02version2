79d9352cb8f80729687a9a4f9517372b7e67f66a
andres2018151725 <andres05022000@gmail.com>
2019-10-26 00:13:58 -0700

    cambios

--- src/routes/index.js
+++ src/routes/index.js
@@ -17,13 +17,25 @@ router.get('/airports/new', (req, res) => {
     res.render('employees/signUpEmployees');
   });
 
-  router.post('/employees/signUpEmployees', (req, res) => {
+    router.post('/employees/signUpEmployees', (req, res) => {
     res.render('employees/signUpEmployees');
-  });
+});
 
   
   router.get('/airports/all', (req, res) => {
     res.render('airports/all-airports');
+  });
+
+  router.get('/flights/createFlight', (req, res) => {
+    res.render('flights/createFlight');
+  });
+
+  router.post('/flights/createFlight', (req, res) => {
+    res.render('flights/createFlight');
+  });
+
+  router.get('/airlines/createAirline', (req, res) => {
+    res.render('airlines/createAirline');
   });
 module.exports = router;
 
