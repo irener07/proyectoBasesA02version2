a4d29a8a17db8dd129ca888e3ab0410ed1f47c5e
Ronaldo Picado Vega <ronaldopv08@gmail.com>
2019-10-27 18:27:49 -0600

    a

--- src/configuration/connectDB.js
+++ src/configuration/connectDB.js
@@ -2,5 +2,6 @@ module.exports = {
     PORT: process.env.PORT || 3000,
     mongoURI: 'mongodb+srv://appUser:Abcd1234@cluster0-tjh3w.mongodb.net/TECPlane?retryWrites=true&w=majority',
     idUserConnected : '',
-    typeUser: ''
+    typeUser: '',
+    idFlight: ''
 }
