e9d8eed326fdf116d664591267bd21c9ba9a6cc0
andres2018151725 <andres05022000@gmail.com>
2019-10-29 16:54:15 -0700

    Flights CRUD

--- src/routes/flights.js
+++ src/routes/flights.js
@@ -26,7 +26,13 @@ router.post('/flights/createFlight', async (req, res) => {
     }
     else{
         const idC = await flights.findOne().sort({$natural:-1}).limit(1);
-        const id = idC.id + 1;
+        var id = 0;
+        if (!idC){
+            id = 1;   
+        }
+        else{   
+            id = idC.id + 1;
+        }
         const ticketsSold = 0;
         const seatNumber = 0;
         console.log (id);
@@ -51,46 +57,22 @@ router.get('/flights', async (req,res) => {
 
 //              Modify flights
 router.get('/flights/modify/:id', async (req, res) => {
+    const airlinesFound = await airlines.find();
     const flightFound = await flights.findById(req.params.id);
-    res.render('flights/editFlights', {flightFound});
+    res.render('flights/editFlight', {flightFound, airlinesFound});
 });
 
 router.put('/flights/modify-flight/:id', async (req,res) => {
-    const {id, idAirline, name, origin, destination, 
+    
+    const { idAirline, name, origin, destination, 
         itinerary, dateTime, restrictions, 
         services, status, maximumCapacity,  tickectsSold, seatNumber, price}= req.body;
     await flights.findByIdAndUpdate(req.params.id, {
-        id, idAirline, name, origin, destination, 
+         idAirline, name, origin, destination, 
             itinerary, dateTime, restrictions, 
             services, status, maximumCapacity,  tickectsSold, seatNumber, price
     });
-    res.redirect('/flights');
-});
-
-
-module.exports = router;
-
-
-/*
-const express = require('express');
-const router = express.Router();
-const flights = require('../models/flights');
-const airlines = require('../models/airlines');
-
-// MODIFICAR UN VUELO
-router.get('/flights/modify/:id', async (req, res) => {
-    const flightFound = await flights.findById(req.params.id);
-    res.render('flights/editFlights', {flightFound});
-});
-
-router.put('/flights/modify-flight/:id', async (req,res) => {
-    const {id, idAirline, name, origin, destination, itinerary, dateTime, price, 
-    restrictions, services, status, maximumCapacity, ticketsSold, seatNumber}= req.body;
-    await flights.findByIdAndUpdate(req.params.id, {
-        id, idAirline, name, origin, destination, itinerary, dateTime, price, 
-    restrictions, services, status, maximumCapacity, ticketsSold, seatNumber
-    });
-    res.redirect('/flights');
+    res.redirect('/employees/moduleManagers');
 });
 
 // BORRAR UN VUELO
@@ -99,49 +81,7 @@ router.delete('/flights/delete/:id', async (req, res) => {
     res.redirect('/flights');
 });
 
-router.get('/flights/moduleFlights', (req, res) => {
-    res.render('flights/moduleFlights');
-});
-
-// CREAR UN VUELO
-router.get('/flights/createFlight', (req, res) => {
-    res.render('flights/createFlight', {airlinesFound});
-  });
-
-//Flights(id, idAirline, name, origin, destination, itinerary{}, date, time, price, 
-//restrictions{}, services{}, status, maximumCapacity, ticketsSold, seatNumber);
-router.post('/flights/createFlight', async (req, res) => {
-    const {name, origin, destination,idAirline, itinerary, dateTime, restrictions, services, status,
-        maximumCapacity, price}= req.body;
-    const errors=[];
-    console.log(req.body); 
-
-   if(id=='' || idAirline=='' || name=='' || origin=='' || destination=='' || itinerary=='' 
-   || dateTime=='' || price=='' || status=='' || maximumCapacity=='' 
-   ){
-        errors.push({text: 'Please, Insert the Data'});
-    }
-    if(errors.length>0){
-        res.render('flights/createFlight',{name, origin, destination,idAirline, itinerary, dateTime, restrictions, services, status,
-            maximumCapacity, price});
-    }
-    else{
-        const idC = await flights.findOne({id: id});
-        if (idC){
-            req.flash('error_msg', 'The ID is Already Registered');
-            res.redirect('/flights/createFlight');
-        }
-        const newFlight = new flights({id, idAirline, name, origin, destination, itinerary, dateTime,
-            price, restrictions, services, status, maximumCapacity});
-        await newFlight.save();
-        req.flash('success_msg', 'Successful Registration');
-        res.redirect('/flights');
-    } 
-
-});
-
-
 
 module.exports = router;
 
-*/
+
