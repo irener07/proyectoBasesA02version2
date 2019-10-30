d42eb654e062f740595a8a1d8ec3a05bb3de81f6
andres2018151725 <andres05022000@gmail.com>
2019-10-29 18:38:17 -0700

    s

--- src/routes/consults.js
+++ src/routes/consults.js
@@ -1,8 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const employees = require('../models/employees');
+const purchases = require('../models/purchases');
+const flights = require('../models/flights');
+const clients = require('../models/clients');
+const airports = require('../models/airports');
+const airlines = require('../models/airlines');
+
 
 
 
 //TERCERA CONSULTA
-router.get('/employees/modify/:id', async (req, res) => {
-    const employeeFound = await employees.findById(req.params.id);
-    res.render('employees/editEmployees', {employeeFound});
+router.get('/employees', async (req,res) => {
+    const employeesFound = await employees.find();
+    res.render('employees/mostVisitedDestinations', {employeesFound});
 });
